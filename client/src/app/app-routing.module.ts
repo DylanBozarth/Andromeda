cb0da9cb35eb64ff49b9b2ba148b966e5cb23209
Dylan <dylanabozarth@gmail.com>
2022-05-01 19:35:26 -0700

    routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { GalaticViewComponent } from './galatic-view/galatic-view.component';
+import { GalaticViewComponent } from './zoom-levels/galatic-view/galatic-view.component';
+import { PlanetViewComponent } from './zoom-levels/planet-view/planet-view.component';
+import { SystemViewComponent } from './zoom-levels/system-view/system-view.component';
 
 const routes: Routes = [
- { path: 'galatic', component: GalaticViewComponent}
+ { path: 'galatic', component: GalaticViewComponent},
+ { path: 'system', component: SystemViewComponent},
+ { path: 'planet', component: PlanetViewComponent}
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GalaticViewComponent } from './galatic-view/galatic-view.component';
-import { SystemViewComponent } from './system-view/system-view.component';
-import { PlanetViewComponent } from './planet-view/planet-view.component';
+import { GalaticViewComponent } from './zoom-levels/galatic-view/galatic-view.component';
+import { SystemViewComponent } from './zoom-levels/system-view/system-view.component';
+import { PlanetViewComponent } from './zoom-levels/planet-view/planet-view.component';
 import { UserNavigationInterfaceComponent } from './components/user-navigation-interface/user-navigation-interface.component';
 
 @NgModule({
