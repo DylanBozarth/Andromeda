ca25659bbe963c1855a9fdf173f0af8b4041283e
Dylan <dylanabozarth@gmail.com>
2022-05-15 16:59:19 -0700

    styles and routes start

--- src/views/sector-view.tsx
+++ src/views/sector-view.tsx
@@ -24,6 +24,74 @@ export const SectorView = () => {
       distanceFromTop: 400,
       distanceFromLeft: 360
     },
+    {
+      systemStar: 'Red-Supergiant',
+      systemName: 'A64122',
+      systemPlanets: [
+        'Gas', 'Gas',
+        'Frozen', 'Frozen',
+        'Lava', 'Frozen',
+        'Gas', 'Frozen'
+      ],
+      distanceFromTop: 400,
+      distanceFromLeft: 360
+    },
+    {
+      systemStar: 'Blue-Giant',
+      systemName: 'A49627',
+      systemPlanets: ['Lava'],
+      distanceFromTop: 463,
+      distanceFromLeft: 163
+    },
+    {
+      systemStar: 'Red-Giant',
+      systemName: 'A50889',
+      systemPlanets: ['Rocky', 'Frozen'],
+      distanceFromTop: 71,
+      distanceFromLeft: 434
+    },
+    {
+      systemStar: 'Yellow-Dwarf',
+      systemName: 'A60984',
+      systemPlanets: ['Frozen', 'Temperate', 'Rocky', 'Lava', 'Ocean'],
+      distanceFromTop: 138,
+      distanceFromLeft: 638
+    },
+    {
+      systemStar: 'Red-Supergiant',
+      systemName: 'A35421',
+      systemPlanets: ['Rocky', 'Ocean', 'Temperate', 'Gas', 'Lava'],
+      distanceFromTop: 13,
+      distanceFromLeft: 899
+    },
+    {
+      systemStar: 'Red-Dwarf',
+      systemName: 'A48843',
+      systemPlanets: ['Rocky', 'Gas'],
+      distanceFromTop: 856,
+      distanceFromLeft: 523
+    },
+    {
+      systemStar: 'Yellow-Dwarf',
+      systemName: 'A63396',
+      systemPlanets: ['Temperate', 'Gas', 'Lava', 'Gas', 'Rocky'],
+      distanceFromTop: 810,
+      distanceFromLeft: 250
+    },
+    {
+      systemStar: 'Blue-Giant',
+      systemName: 'A82856',
+      systemPlanets: ['Rocky'],
+      distanceFromTop: 874,
+      distanceFromLeft: 472
+    },
+    {
+      systemStar: 'Red-Giant',
+      systemName: 'A38881',
+      systemPlanets: [],
+      distanceFromTop: 742,
+      distanceFromLeft: 556
+    },
   ])
 
   {
