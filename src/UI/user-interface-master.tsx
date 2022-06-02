adc6deb5a73904fa78aff005bd392389b09a00d2
Paul Mason <webdevpaulmason@gmail.com>
2022-06-01 20:09:20 -0700

    7: Add increment/decrement to menu options

--- src/UI/user-interface-master.tsx
+++ src/UI/user-interface-master.tsx
@@ -1,38 +1,69 @@
-import { Link } from "react-router-dom"
-import "../styles/user-interface-master.css"
+import { Link } from "react-router-dom";
+import { useAppDispatch } from "../redux/hooks";
+import { decrement, increment } from "../redux/testSlice";
+import "../styles/user-interface-master.css";
 
 export const UImaster: React.FC = () => {
-    return (
-        <div className="UI-master">
+  const dispatch = useAppDispatch();
+  return (
+    <div className="UI-master">
+      <div className="menu">
+        <nav>
+          <ul>
+            <li>
+              <a href="#">
+                <Link to="/" className="ui-main-link">
+                  Galatic View
+                </Link>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <Link to="/sectora" className="ui-main-link">
+                  SECTOR-A
+                </Link>
+              </a>
+            </li>
 
-        
-        <div className="menu">
-      <nav>
-        <ul>
-        <li><a href="#"><Link to="/" className="ui-main-link">Galatic View</Link></a></li>
-          <li><a href="#"><Link to="/sectora" className="ui-main-link">SECTOR-A</Link></a></li>
-          
-          <li><a href="#">menu</a></li>
-         
-          <li><a href="#">dropdown</a>
-            <ul className="sub1">
-              <li><a href="#">menu</a></li>
-              <li><a href="#">menu</a></li>
-              <li><a href="#">menu</a></li>
-              <li><a href="#">menu</a>
-                <ul className="sub2">
-                  <li><a href="#">menu</a></li>
-                  <li><a href="#">menu</a></li>
-                  <li><a href="#">menu</a></li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </nav>
+            <li>
+              <a href="#">menu</a>
+            </li>
+
+            <li>
+              <a href="#">dropdown</a>
+              <ul className="sub1">
+                <li>
+                  <a href="#" onClick={() => dispatch(increment())}>
+                    Increment
+                  </a>
+                </li>
+                <li>
+                  <a href="#" onClick={() => dispatch(decrement())}>
+                    Decrement
+                  </a>
+                </li>
+                <li>
+                  <a href="#">menu</a>
+                </li>
+                <li>
+                  <a href="#">menu</a>
+                  <ul className="sub2">
+                    <li>
+                      <a href="#">menu</a>
+                    </li>
+                    <li>
+                      <a href="#">menu</a>
+                    </li>
+                    <li>
+                      <a href="#">menu</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
-        </div>
-    )
-}
-
-
+  );
+};
