866bbb95b1ad0a0a516f471d9f4939ed4602d1f2
Paul Mason <webdevpaulmason@gmail.com>
2022-06-02 21:30:54 -0700

    8: Implement new functions connecting to Redux store

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import App from "./App";
+import { saveState } from "./redux/localStorage";
+
+store.subscribe(() => saveState(store.getState()));
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
