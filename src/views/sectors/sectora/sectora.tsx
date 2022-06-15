9f4caf4932139b119c36ddd8cab5d481ae12191f
Paul Mason <webdevpaulmason@gmail.com>
2022-06-14 21:41:46 -0700

    1-Setup-sector-view: Setup inline styles for random placement of stars

--- src/views/sectors/sectora/sectora.tsx
+++ src/views/sectors/sectora/sectora.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { Star } from '../../../components/star';
 import componentStyles from '../../../styles/components.module.css';
 import { PlayerSystem, Sector } from '../../../types/system-interfaces';
+import { generateRandomNumber } from '../../../utils/math';
 import { MOCK_SECTOR_ARRAY } from './MOCK_DATA';
 
 interface PlayerSystemProps {
@@ -20,15 +21,21 @@ export const SectorA = ({ playerSystem, setPlayerSystem }: PlayerSystemProps) =>
       <div className={componentStyles['sector-view-wrapper']}>
         {sector.map((sector) => {
           return (
-            <div key={sector.systemName} className={componentStyles['sector-star-wrapper']}>
+            <div
+              key={sector.systemName}
+              style={{
+                position: 'absolute',
+                top: `${generateRandomNumber(20, 80)}vh`,
+                left: `${generateRandomNumber(20, 80)}vw`,
+              }}
+              className={componentStyles['sector-star-wrapper']}
+            >
               <Link to={`/system/${sector.systemName}`} onClick={() => setPlayerSystem({ sector })}>
                 <Star systemName={sector.systemName} systemStar={sector.systemStar} />
               </Link>
             </div>
           );
         })}
-        Eventually I want these to look more random in their placements so there can be clusters,
-        etc.
       </div>
     );
   }
