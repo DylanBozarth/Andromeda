a460ad35d3f34d188049fe2ffa381a612848b335
Dylan <dylanabozarth@gmail.com>
2022-07-01 17:41:50 -0700

    navigation and bottom bar

--- src/UI/navigation-bar.tsx
+++ src/UI/navigation-bar.tsx
@@ -22,12 +22,12 @@ export const NavigationBar = () => {
   }, [location])
   return (
     <div className={styles['navigation-bar']}>
-      <Link to='/' onClick={() => setUserSector('')} className={styles['navigation-bar-text']}><div className={styles['navigation-bar-section']}>Galaxy</div></Link>
+      <Link to='/' onClick={() => setUserSector('')} className={styles['navigation-bar-text']}><div className={styles['navigation-bar-section']}>Andromeda</div></Link>
       <Link to={`/${userSector}`} className={styles['navigation-bar-text']}><div className={userSector ? styles['navigation-bar-section'] : styles['navigation-bar-section-inactive']} onClick={() => setUserSystem('')}>
-        {userSector}
+        Sector: {userSector}
       </div></Link>
       <div className={userSystem ? styles['navigation-bar-section'] : styles['navigation-bar-section-inactive']}>
-        <p className={styles['navigation-bar-text']}>{userSystem}</p>
+        <p className={styles['navigation-bar-text']}>System:{userSystem}</p>
       </div>
     </div>
   )
--- src/UI/system-bottom-bar.tsx
+++ src/UI/system-bottom-bar.tsx
@@ -10,18 +10,15 @@ export const SystemBottomBar = ({ playerSystem }: systemInformation) => {
         console.log('from the bar', playerSystem)
     }, [])
     return (
-        <div>
-        <div className={styles['system-bottom-bar']}>
-            <div className={styles['bottom-bar-section']}></div>
-<div className={styles['bottom-bar-section']}>Buildings in Progress
-<br />
-Love shack-planet 5, 78% 4hr 15min remaining</div>
-<div className={styles['bottom-bar-section']}>System Power 
-<br /> Hangar 700<br /> Planetary defenses 3000</div>
+        <div><h4 className={styles['system-title']}>{playerSystem.systemName}, {playerSystem.systemStar} system. <br />
+                {playerSystem.ownership !== 'unowned' ? `${playerSystem.ownership}'s system` : 'Unclaimed System'}</h4>
+            <div className={styles['system-bottom-bar']}>
+               
+                <div className={styles['bottom-bar-section']}>System Power
+                    <br /> Hangar 700<br /> Planetary defenses 3000</div>
 
-        </div>
-        <h4 className={styles['system-title']}>{playerSystem.systemName}, {playerSystem.systemStar} system. <br />
-        {playerSystem.ownership !== 'unowned' ? `${playerSystem.ownership}'s system` : 'Unclaimed System'}</h4>
+            </div>
+            
         </div>
     )
 }
