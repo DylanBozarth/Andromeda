469ca741373b46575d7cfd5b5dd8db0aae3a299e
Dylan <dylanabozarth@gmail.com>
2022-07-24 13:20:52 -0700

    side bar prototype

--- src/UI/system-side-bar.tsx
+++ src/UI/system-side-bar.tsx
@@ -1,7 +1,7 @@
 import styles from '../styles/user-interface-master.module.css'
 import { System } from '../utils/system-generator/generate-sector';
 import { useState } from 'react';
-import { SystemBottomBar } from './system-bottom-bar';
+import { SystemPowerBar } from './system-power-bar';
 interface systemInformation {
     playerSystem: System;
 }
@@ -16,8 +16,12 @@ export const SystemSideBar = ({ playerSystem, toggleBuildings, toggleResources, 
     const [tabNumber, setTabNumber] = useState(1)
     return (
         <div className={styles['side-bar-wrapper']}>
+
             <div onClick={() => setExpanded(!expanded)} className={`${styles['ui-border-box']} ${styles['toggle-side-bar']}`}>Toggle</div>
             <div className={expanded ? styles['side-bar'] : 'hidden'}>
+                <div className={styles['side-bar-background-wrapper']}>
+                    <div className={styles['side-bar-background']}></div>
+                </div>
                 <div className={styles['side-tab-row']}>
                     <div className={styles['side-tab']} onClick={() => setTabNumber(1)}>Buildings</div>
                     <div className={styles['side-tab']} onClick={() => setTabNumber(2)}>Hangar</div>
@@ -27,7 +31,7 @@ export const SystemSideBar = ({ playerSystem, toggleBuildings, toggleResources, 
                     <div className={tabNumber === 2 ? `${styles['side-tab-info']}` : 'hidden'}>Production</div>
                     <div className={tabNumber === 3 ? `${styles['side-tab-info']}` : 'hidden'}>Alerts</div>
                 </div>
-                <SystemBottomBar playerSystem={playerSystem} toggleResources={toggleResources} toggleBuildings={toggleBuildings} setToggleResources={setToggleResources} setToggleBuildings={setToggleBuildings} />
+                <SystemPowerBar playerSystem={playerSystem} toggleResources={toggleResources} toggleBuildings={toggleBuildings} setToggleResources={setToggleResources} setToggleBuildings={setToggleBuildings} />
             </div>
         </div>
     )
--- src/views/galatic-view.tsx
+++ src/views/galatic-view.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/views-styles/galatic-view.module.css';
 export const GalaticView = () => {
   return (
     <div>
-      <img src='./assets/Galaxy.jpg' className={styles.galaxy} alt='galaxy' />
+      <img src='./assets/Galaxy.jpg' className={styles['galaxy']} alt='galaxy' />
       <div className={styles['sectorawrapper']}><img src='./assets/sample-image.png'></img><Link to='/redux-sector' className={styles['sectora']}> SECTOR A</Link>
       </div>
     </div>
