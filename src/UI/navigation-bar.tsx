105c5483d4ea19dfecd20eb76bf2f7ba4c442cde
Paul Mason <webdevpaulmason@gmail.com>
2022-09-02 21:12:36 -0700

    46-Almost fixed glitch

--- src/UI/navigation-bar.tsx
+++ src/UI/navigation-bar.tsx
@@ -1,38 +1,56 @@
 import styles from '../styles/user-interface-master.module.css';
 import { Link, useLocation } from 'react-router-dom';
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import { useAppSelector } from '../redux/hooks';
 
 export const NavigationBar = () => {
-  const [userSector, setUserSector] = useState('');
-  const [userSystem, setUserSystem] = useState('');
+  const [userSector, setUserSector] = useState(
+    useAppSelector((state) => state.sector.activeSector.sectorName),
+  );
+  const [userSystem, setUserSystem] = useState(
+    useAppSelector((state) => state.sector.activeSystem.systemName),
+  );
   const location = useLocation();
   useEffect(() => {
     if (window.location.href.includes('system')) {
-      const locationURL = (window.location.href.split('/'))
-      setUserSystem(locationURL[locationURL.length - 1])
-    }
-    else if (window.location.href.includes('sector')) {
-      const locationURL2 = (window.location.href.split('/'))
-      setUserSector(locationURL2[locationURL2.length - 1])
-    }
-    else {
+      const locationURL = window.location.href.split('/');
+      setUserSystem(locationURL[locationURL.length - 1]);
+    } else if (window.location.href.includes('sector')) {
+      const locationURL2 = window.location.href.split('/');
+      setUserSector(locationURL2[locationURL2.length - 1]);
+    } else {
       setUserSector('');
-      setUserSystem('')
+      setUserSystem('');
     }
-  }, [location])
+  }, [location]);
   return (
     <div className={styles['navigation-bar']}>
       <Link to='/' onClick={() => setUserSector('')} className={styles['navigation-bar-text']}>
-          <div className={`${styles['navigation-bar-section']} ${styles['ui-border-box']}`}><div className={styles['navigation-bar-text']}>Andromeda</div></div>
+        <div className={`${styles['navigation-bar-section']} ${styles['ui-border-box']}`}>
+          <div className={styles['navigation-bar-text']}>Andromeda</div>
+        </div>
       </Link>
       <Link to={`/${userSector}`} className={styles['navigation-bar-text']}>
-        <div className={userSector ? `${styles['navigation-bar-section']} ${styles['ui-border-box']}` : styles['navigation-bar-section-inactive']} onClick={() => setUserSystem('')}>
-        <div className={styles['navigation-bar-text']}>{userSector}</div>
-      </div>
+        <div
+          className={
+            userSector
+              ? `${styles['navigation-bar-section']} ${styles['ui-border-box']}`
+              : styles['navigation-bar-section-inactive']
+          }
+          onClick={() => setUserSystem('')}
+        >
+          <div className={styles['navigation-bar-text']}>{userSector}</div>
+        </div>
       </Link>
-      <div className={userSystem ? `${styles['navigation-bar-section']} ${styles['ui-border-box']}` : styles['navigation-bar-section-inactive']}>
+      <div
+        className={
+          userSystem
+            ? `${styles['navigation-bar-section']} ${styles['ui-border-box']}`
+            : styles['navigation-bar-section-inactive']
+        }
+      >
         <div className={styles['navigation-bar-text']}>{userSystem}</div>
       </div>
     </div>
-  )
-}
+  );
+};
