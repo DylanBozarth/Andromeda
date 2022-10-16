d5dfa93c86c7c7ac9bdfe1f1bebe76f2b3fe72b1
Dylan <dylanabozarth@gmail.com>
2022-10-16 14:33:10 -0700

    I guess it's ok

--- src/UI/menu-pages/pop-ups/resources.tsx
+++ src/UI/menu-pages/pop-ups/resources.tsx
@@ -1,11 +1,12 @@
 import '../../../styles/menus/pop-ups.scss'
 import '../../../styles/menus/menu-base-styles.css'
 import { playerRawResources } from '../../../player-data/raw-resources'
+import { playerRefinedResources } from '../../../player-data/refined-resources'
+import { playerRareResources } from '../../../player-data/rare-resources'
 export const ResourceMenu = () => {
     return (
         <div className='resource-menu'>
 
-            <h1 className='resource-menu-title'>Resources </h1>
             {/* the big 4 */}
             <div className="first-line">
                 <h4>Raw resources</h4>
@@ -15,12 +16,26 @@ export const ResourceMenu = () => {
                 <p>Ore : {playerRawResources.ore}</p>
             </div>
             <div className='second-line'>
-                <h4>Refinded resources</h4>
-
-
+                <h4>Refined resources</h4>
+                <p>Approval: {playerRefinedResources.approval}</p>
+                <p>Energy: {playerRefinedResources.energy}</p>
+                <p>Fuel: {playerRefinedResources.fuel}</p>
+                <p> Alloys: {playerRefinedResources.alloys}</p>
+                <p>Ceramics and Plastics: {playerRefinedResources['ceramics-and-plastics']}</p>
+                <p>Metals: {playerRefinedResources.metals}</p>
+                <p>Delicacies: {playerRefinedResources.delicacies}</p>
+                <p>Consumer Goods: {playerRefinedResources['consumer-goods']}</p>
             </div>
             <div className='third-line'>
                 <h4>Unique resources</h4>
+                <p>Sector-a-crystals: {playerRareResources['sector-a-crystals']}</p>
+                <p>Sector-b-crystals: {playerRareResources['sector-b-crystals']}</p>
+                <p>Sector-c-crystals: {playerRareResources['sector-c-crystals']}</p>
+                <p>Sector-d-crystals: {playerRareResources['sector-d-crystals']}</p>
+                <p>Sector-e-crystals: {playerRareResources['sector-e-crystals']}</p>
+                <p>Sector-f-crystals: {playerRareResources['sector-f-crystals']}</p>
+                <p>Sector-g-crystals: {playerRareResources['sector-g-crystals']}</p>
+                <p>Sector-h-crystals: {playerRareResources['sector-h-crystals']}</p>
             </div>
 
         </div>
