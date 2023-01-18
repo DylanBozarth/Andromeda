a02b58df5cbba5dafdd22ef7728a007ee44aeed6
Dylan <dylanabozarth@gmail.com>
2023-01-17 21:21:01 -0800

    yeah baby

--- src/UI/user-interface-master.tsx
+++ src/UI/user-interface-master.tsx
@@ -1,7 +1,6 @@
 import { useSystems } from '../utils/system-generator/redux-hook';
 import { TopBar } from './top-bar';
 import { NavigationBar } from './navigation-bar';
-import '../styles/tailwind.css'
 export const UImaster: React.FC = () => {
   const generateSystems = useSystems(10, 8);
   return (
@@ -12,7 +11,7 @@ export const UImaster: React.FC = () => {
           Generate new sector
         </a>
       </li>
-      <h1 className='text-3xl font-bold underline'>Tailwind test</h1>
+      <h1 className='text-3xl font-bold underline'>Tailwind, am I bold?</h1>
       <NavigationBar />
     </div>
   );
