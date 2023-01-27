b4778f753423e97eed2b9ae5506a605a7dcfb378
Dylan <dylanabozarth@gmail.com>
2023-01-26 17:39:15 -0800

    line removed

--- src/UI/user-interface-master.tsx
+++ src/UI/user-interface-master.tsx
@@ -11,7 +11,6 @@ export const UImaster: React.FC = () => {
           Generate new sector
         </a>
       </li>
-      <h1 className='text-3xl font-bold underline'>Tailwind, am I bold?</h1>
       <NavigationBar />
     </div>
   );
