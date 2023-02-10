856138d1d015d7b4a1668aca31a4ab82f7b34744
Dylan <dylanabozarth@gmail.com>
2023-02-09 18:48:24 -0800

    before removing declarations

--- src/UI/menu-pages/detailsMenu.tsx
+++ src/UI/menu-pages/detailsMenu.tsx
@@ -1,4 +1,4 @@
-import styles from '../../styles/user-interface-master.module.scss';
+import '../../styles/user-interface-master.module.scss';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { SystemDetails } from './details-pages/system-management';
@@ -10,41 +10,41 @@ import { ExploreDetails } from './details-pages/exploration';
 export const DetailsMenu = () => {
   const [openMenu, setOpenMenu] = useState<string>('systems');
   return (
-    <div className={styles['details-menu-wrapper']}>
-      <div className={styles['nav-container']}>
-        <div className={styles['details-bar-wrapper']}>
-          <div className={styles['details-nav-button-base']}>
-            <div className={styles['details-nav-1']} onClick={() => setOpenMenu('systems')}>
+    <div className='details-menu-wrapper'>
+      <div className='nav-container'>
+        <div className='details-bar-wrapper'>
+          <div className='details-nav-button-base'>
+            <div className='ui-border-box' onClick={() => setOpenMenu('systems')}>
               System Management{' '}
             </div>
           </div>
-          <div className={styles['details-nav-button-base']}>
-            <button className={styles['details-nav-1']} onClick={() => setOpenMenu('fleets')}>
+          <div className='details-nav-button-base'>
+            <button className='ui-border-box' onClick={() => setOpenMenu('fleets')}>
               Fleets
             </button>
           </div>
-          <div className={styles['details-nav-button-base']}>
-            <button className={styles['details-nav-1']} onClick={() => setOpenMenu('research')}>
+          <div className='details-nav-button-base'>
+            <button className='ui-border-box' onClick={() => setOpenMenu('research')}>
               research
             </button>
           </div>
-          <div className={styles['details-nav-button-base']}>
-            <button className={styles['details-nav-1']} onClick={() => setOpenMenu('espionage')}>
+          <div className='details-nav-button-base'>
+            <button className='ui-border-box' onClick={() => setOpenMenu('espionage')}>
               Espionage
             </button>
           </div>
-          <div className={styles['details-nav-button-base']}>
-            <button className={styles['details-nav-1']} onClick={() => setOpenMenu('exploration')}>
+          <div className='details-nav-button-base'>
+            <button className='ui-border-box' onClick={() => setOpenMenu('exploration')}>
               Exploration
             </button>
           </div>
-          <div className={styles['details-nav-button-base']}>
-            <div className={styles['details-nav-1']} onClick={() => setOpenMenu('ship-designer')}>
+          <div className='details-nav-button-base'>
+            <div className='ui-border-box' onClick={() => setOpenMenu('ship-designer')}>
               Ship designer
             </div>
           </div>
         </div>
-        <Link to='/' className={styles['go-back']}>
+        <Link to='/' className='go-back'>
           Go back aaaaa
         </Link>
       </div>
