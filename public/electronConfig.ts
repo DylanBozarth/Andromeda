bdc1a4eb08a52de0f984452502b5b6e4f4e379f0
Dylan <dylanabozarth@gmail.com>
2023-03-25 14:53:14 -0700

    LMAO LETS GOOOO

---
+++ public/electronConfig.ts
@@ -0,0 +1,44 @@
+const { app, BrowserWindow, screen: electronScreen } = require('electron');
+const path = require('path');
+
+const createMainWindow = () => {
+  let mainWindow = new BrowserWindow({
+    width: electronScreen.getPrimaryDisplay().workArea.width,
+    height: electronScreen.getPrimaryDisplay().workArea.height,
+    show: false,
+    backgroundColor: 'white',
+    webPreferences: {
+      nodeIntegration: false,
+    }
+  });
+  const startURL = 'http://localhost:3000'
+
+  mainWindow.loadURL(startURL);
+
+  mainWindow.once('ready-to-show', () => mainWindow.show());
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+
+  mainWindow.webContents.on('new-window', (event, url) => {
+    event.preventDefault();
+    mainWindow.loadURL(url);
+  });
+};
+
+app.whenReady().then(() => {
+  createMainWindow();
+
+  app.on('activate', () => {
+    if (!BrowserWindow.getAllWindows().length) {
+      createMainWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
