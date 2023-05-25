0e175adc8f92ec2fc37ee6d2a6a6aec7b511894e
Dylan <dylanabozarth@gmail.com>
2023-05-24 17:43:24 -0700

    NCO-view

---
+++ src/components/bigNCO.tsx
@@ -0,0 +1,26 @@
+import '../styles/textures/planets-and-stars.scss';
+import { NCO } from '../utils/system-generator/generate-sector';
+interface NCOProps {
+  NCO: NCO;
+}
+
+export const BigNCOComponent = ({ NCO }: NCOProps) => {
+  return (
+    <div className={`big-NCO ${NCO.name}`}>
+      {NCO.name}
+    </div>
+  );
+};
+
+/* NCO effects  
+heat levels, max 3. Higher heat is good for industry, lower heat is good for science and living areas
+Frozen: heat level decrease(2), approval debuff
+Lava: heat level increase(2), approval debuff, large ore refining bonus 
+gas: approval debuff, heat level decrease(2), science bonus
+asteroid belt: heat level decrease(3), ore collection (not refining) bonus
+temperate: Approval bonus
+ocean: heat level decrease, approval bonus, water bonus
+desert: approval debuff, heat level increase(1), ore collection (not refining) bonus
+greenhouse: approval bonus, biomass bonus, science bonus
+rocky: water debuff, ore collection (not refining) bonus, ore refining bonus
+*/
--- src/views/NCO-view.tsx
+++ src/views/NCO-view.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import { useAppSelector } from '../redux/hooks';
+import { BigNCOComponent } from '../components/bigNCO';
 export const NCOView = () => {
     const playerNCO = useAppSelector((state) => state.sector.activeNCO);
     useEffect(() => {
@@ -7,7 +8,7 @@ export const NCOView = () => {
     }, [])
     return (
         <div className="absolute text-center bold p-20">
-            pretend theres a black hole here or something, we dont have the images yet.  <br />
+            <BigNCOComponent NCO={playerNCO} />
             A {playerNCO.name} will be shown here. And it will have {playerNCO.effect} on all fleets who battle in the system
         </div>
     )
