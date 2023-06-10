104a40650de2c857562c2e4ec17bb01d83b5a923
Dylan <dylanabozarth@gmail.com>
2023-06-10 15:27:50 -0700

    cursed data fetch

--- src/views/sectors/sector-a.tsx
+++ src/views/sectors/sector-a.tsx
@@ -5,10 +5,26 @@ import { setSystem, setNCO } from '../../redux/sectorSlice';
 import { getXfromCords, getYfromCords } from '../../utils/system-generator/system-functions';
 import { NCOComponent } from '../../components/NCO';
 import '../../styles/views-styles/sector-view.css'
+import { useEffect } from 'react';
 
 export const SectorA = () => {
   const dispatch = useAppDispatch();
   const sector = useAppSelector((state) => state.sector.activeSector);
+  useEffect(() => {
+    fetch('https://andromeda-backend-production.up.railway.app/api/sectors')
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(
+          `This is an HTTP error: The status is ${response.status}`
+        );
+      }
+      return response.json();
+    })
+    .then((actualData) => console.log(actualData.data[0].attributes.sectorA))
+    .catch((err) => {
+      console.log(err.message);
+    });
+  }, [])
   {
     return (
       <div className=''>
