27181bb3681897d4236646311683536107418922
Dylan <dylanabozarth@gmail.com>
2023-08-15 21:23:41 -0700

    side bar is nicer

--- src/UI/side-bars/planet-side-bar.tsx
+++ src/UI/side-bars/planet-side-bar.tsx
@@ -5,19 +5,13 @@ import { FleetFloatMenu } from '../float-menus/fleets';
 
 interface toggles {
     playerPlanet: Planet;
-    toggleResources: boolean;
-    toggleBuildings: boolean;
-    setToggleResources: (flag: boolean) => void;
-    setToggleBuildings: (flag: boolean) => void;
 }
 export const PlanetSideBar = ({
-    playerPlanet,
-    toggleBuildings,
-    toggleResources,
-    setToggleBuildings,
-    setToggleResources
+    playerPlanet
 }: toggles) => {
     const [tabNumber, setTabNumber] = useState(1);
+    const [toggleResources, setToggleResources] = useState(false);
+    const [toggleBuildings, setToggleBuildings] = useState(false);
     return (
         <div className='middle-layer-menu'>
             <div className='side-bar p-1 ui-white-box'>
@@ -38,26 +32,52 @@ export const PlanetSideBar = ({
                 </div>
                 <div className='side-screen p-1'>
                     <div className={tabNumber === 1 ? 'side-tab-info' : 'hidden'}>
-                        <div className='m-2 text-center border-2 flex'><p className='p-2'>ICON</p><p className='p-2'>A building is building on planet X</p></div>
+                        <div className='m-2 text-center border-2 flex'><p>Production: {playerPlanet.production}</p></div>
                     </div>
                     <div className={tabNumber === 2 ? 'side-tab-info' : 'hidden'}>
-                        <div className='m-2 text-center border-2 flex'><p className='p-2'>Status</p><p className='p-2'>SHIP</p></div>
+                        <div className='m-2 text-center border-2 flex'><p>Hangar:</p>{playerPlanet.hangar}</div>
                     </div>
                     <div className={tabNumber === 3 ? 'side-tab-info' : 'hidden'}>
-                        <div className='m-2 text-center border-2 flex'><p className='p-2'>Like 6 rocks</p></div>
+                        <div className='m-2 text-center border-2 flex'><p>Resources in storage:</p>{playerPlanet.resourceStorage}</div>
                     </div>
                 </div>
-                
-                <div className='text-center p-1 ui-border-box'>
-                {playerPlanet.ownership === 'unowned' ? 'Claim this planet' : ''}
-                </div>
+
+
                 <FleetFloatMenu />
                 <div className='flex p-1 justify-center'>
-                    <div onClick={() => setToggleBuildings(!toggleBuildings)} className='ui-border-box p-2'>Buildings</div>
-                    <div onClick={() => setToggleResources(!toggleResources)} className='ui-border-box p-2'>Desposits</div>
+                    <div onClick={() => setToggleBuildings(!toggleBuildings)} className=' p-1'>{toggleBuildings ? <p>{playerPlanet.buildings.length} Buildings</p> : 'Buildings'}</div>
+                    <div onClick={() => setToggleResources(!toggleResources)} className=' p-1'>Desposits</div>
+                </div>
+                {[
+                    playerPlanet.naturalResources.map((resource, idx) => {
+                        return (
+                            <div
+                                className={toggleResources ? 'planet-resources mt-10 border-1 p-10 ' : 'hidden'}
+                                key={`${playerPlanet}-${resource}-${idx}`}
+                            >
+                                <div className=''>{resource}</div>
+                            </div>
+                        );
+                    }),
+                ]}
+                {[
+                    playerPlanet.buildings.map((building, idx) => {
+                        return (
+                            <div
+                                className={toggleBuildings ? 'planet-buildings mt-5' : 'hidden'}
+                                key={`${playerPlanet}-${building}-${idx}`}
+                            >
+                                {building}
+
+                            </div>
+                        );
+                    }),
+                ]}
+                <div className='text-center p-4 ui-border-box'>
+                    {playerPlanet.ownership === 'unowned' ? 'Claim this planet' : ''}
                 </div>
             </div>
-            
+
         </div>
     );
 };
--- src/views/planet-view.tsx
+++ src/views/planet-view.tsx
@@ -7,8 +7,6 @@ import { PlanetSideBar } from '../UI/side-bars/planet-side-bar';
 export const PlanetView = () => {
   const playerPlanetArray = useAppSelector((state) => [state.sector.activeSystem.activePlanet]);
   const playerPlanet = useAppSelector((state) => state.sector.activeSystem.activePlanet);
-  const [toggleResources, setToggleResources] = useState(false);
-  const [toggleBuildings, setToggleBuildings] = useState(false);
   useEffect(() => {
     console.log(playerPlanet)
   }, [])
@@ -19,40 +17,8 @@ export const PlanetView = () => {
           <div key="planet-view" className='flex'>
             <div className={'planet-wrapper'}>
               <PlanetSideBar
-                playerPlanet={playerPlanet}
-                toggleResources={toggleResources}
-                toggleBuildings={toggleBuildings}
-                setToggleResources={setToggleResources}
-                setToggleBuildings={setToggleBuildings} />
+                playerPlanet={playerPlanet} />
               <BigPlanetComponent planet={planet} />
-            </div>
-            <div className=''>
-              {[
-                planet.naturalResources.map((resource, idx) => {
-                  return (
-                    <div
-                      className={toggleResources ? 'planet-resources mt-10 border-1 p-10 ' : 'hidden'}
-                      key={`${planet}-${resource}-${idx}`}
-                    >
-                      <div className=''>{resource}</div>
-                    </div>
-                  );
-                }),
-              ]}
-
-              {[
-                planet.buildings.map((building, idx) => {
-                  return (
-                    <div
-                      className={toggleBuildings ? 'planet-buildings mt-5' : 'hidden'}
-                      key={`${planet}-${building}-${idx}`}
-                    >
-                      {building}
-
-                    </div>
-                  );
-                }),
-              ]}
             </div>
           </div>
         );
