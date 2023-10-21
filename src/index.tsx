225f97d089a8db5521a52537099685267f23bb11
Dylan <dylanabozarth@gmail.com>
2023-10-21 14:45:58 -0700

    clean console

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './App';
 import { saveState } from './redux/localStorage';
-import AuthProvider from './non-game-pages/AuthProvider/AuthProvider';
+// import AuthProvider from './non-game-pages/AuthProvider/AuthProvider';
 
 store.subscribe(() => saveState(store.getState()));
 
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <AuthProvider>
+
         <App />
-      </AuthProvider>
+     
     </Provider>
   </React.StrictMode>,
 );
